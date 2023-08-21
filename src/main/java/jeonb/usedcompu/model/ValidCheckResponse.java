eca7258f51064cd8169a84d04de6581a1727887a
JeonB <newjeonb@gmail.com>
2023-08-21 23:33:53 +0900

    New features Cart: 장바구니 entity 추가

--- src/main/java/jeonb/usedcompu/model/ValidCheckResponse.java
+++ src/main/java/jeonb/usedcompu/model/ValidCheckResponse.java
@@ -1,10 +1,12 @@
 package jeonb.usedcompu.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 @AllArgsConstructor
+@Getter
+@Setter
 public class ValidCheckResponse {
 
     private Boolean status;
